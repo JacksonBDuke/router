21f84ec5093f1d6bebb4fc7ca1b2779d2ac0fdb6
Jackson Duke <Jackson Duke>
2016-12-03 19:30:11 -0600

    Testing

--- sr_router.c
+++ sr_router.c
@@ -560,7 +560,7 @@ void sr_handlepacket(struct sr_instance* sr,
     sr_handlepacket_arp(sr, pkt, len, inf_cpy);
   } else if (type == ethertype_ip) {
     /* STILL NEED TO ADD THIS*/
-	sr_handlepacket_ip(sr, pckt, len);
+	sr_handlepacket_ip(sr, pkt, len);
   } else {
     fprintf(stderr, "invalid packet type id in ethernet header\n");
   }
