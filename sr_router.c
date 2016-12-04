959afa302539256565d4fffee4d0b0911488ccc2
Jackson Duke <Jackson Duke>
2016-12-03 18:23:35 -0600

    Update, added some trouble-shooting printf

--- sr_router.c
+++ sr_router.c
@@ -437,9 +437,9 @@ void sr_handlepacket_arp(struct sr_instance *sr, uint8_t *pkt,
 		{
 			printf("Request is for me...\n");
 			sr_send_arpreply(sr, pkt, len, src_iface); }
+		/*Added for testing.*/
 		else{
 			printf("Request isn't for me.\n");
-			sr_handle_arpreq(sr, pkt, src_iface);
 		}
 		break;
 	  }
